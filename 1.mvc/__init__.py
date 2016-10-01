01108e2675211f086aebba652a7ab0c18f826147
Svintsov Dmitry <root@uralbash.ru>
2016-10-01 10:24:24 +0500

    Fixed mvc section

--- 1.mvc/__init__.py
+++ 1.mvc/__init__.py
@@ -1,18 +1,8 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2015 uralbash <root@uralbash.ru>
-#
-# Distributed under terms of the MIT license.
+from views import BlogRead, BlogIndex, BlogCreate, BlogDelete, BlogUpdate
+from wsgi_basic_auth import BasicAuth
 
-"""
-Simple blog
-"""
-from paste.auth.basic import AuthBasicHandler
-
+# third-party
 import selector
-from views import BlogCreate, BlogDelete, BlogIndex, BlogRead, BlogUpdate
 
 
 def authfunc(environ, username, password):
@@ -20,10 +10,13 @@ def authfunc(environ, username, password):
 
 
 def make_wsgi_app():
+    passwd = {
+        'admin': '123'
+    }
     # BasicAuth applications
-    create = AuthBasicHandler(BlogCreate, 'www', authfunc)
-    update = AuthBasicHandler(BlogUpdate, 'www', authfunc)
-    delete = AuthBasicHandler(BlogDelete, 'www', authfunc)
+    create = BasicAuth(BlogCreate, 'www', passwd)
+    update = BasicAuth(BlogUpdate, 'www', passwd)
+    delete = BasicAuth(BlogDelete, 'www', passwd)
 
     # URL dispatching middleware
     dispatch = selector.Selector()
--- 1.mvc/default.nix
+++ 1.mvc/default.nix
@@ -21,9 +21,55 @@ let
       homepage = http://pythonpaste.org/;
     };
   };
+
+  resolver = pythonPackages.buildPythonPackage rec {
+    name = "resolver-0.2.1";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/resolver-0.2.1.tar.gz;
+      sha256 = "1hn709w0bpfg77s7pqrvfd2wlqnjmmm5vn8gzqid40jkahn15lvh";
+    };
+
+    buildInputs = with pythonPackages; [];
+
+    doCheck = false; # some files required by the test seem to be missing
+  };
+
+  selector = pythonPackages.buildPythonPackage rec {
+    name = "selector-0.10.1";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/selector-0.10.1.tar.gz;
+      sha256 = "03sq1dhll6k84s6acb57wrbc0x959s83bp2r5fysja5x6dwsiqrc";
+    };
+
+    buildInputs = with pythonPackages; [ resolver ];
+
+    doCheck = false; # some files required by the test seem to be missing
+
+    meta = {
+      description = "This distribution provides WSGI middleware for “RESTful”
+      mapping of URL paths to WSGI applications";
+      homepage = "http://github.com/lukearno/selector/";
+    };
+  };
+  wsgi-basic-auth = pythonPackages.buildPythonPackage rec {
+    name = "wsgi-basic-auth-1.0.4";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/wsgi-basic-auth-1.0.4.tar.gz;
+      sha256 = "18dmkb0cazdpawnj4xp49ss70lq3mpnka7nfvg9y26ampfn6b5p5";
+    };
+
+    buildInputs = with pythonPackages; [ pythonPackages.webob ];
+
+    doCheck = false; # some files required by the test seem to be missing
+  };
 in rec {
   pyEnv = stdenv.mkDerivation {
     name = "py-paste";
-    buildInputs = [ stdenv python3 paste3 pythonPackages.ipdb ];
+    buildInputs = [ stdenv python3 paste3 ]
+      ++ [ resolver selector wsgi-basic-auth ]
+      ++ [ pythonPackages.webob pythonPackages.ipdb ];
   };
 }
