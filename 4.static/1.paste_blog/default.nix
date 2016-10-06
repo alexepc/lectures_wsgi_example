eca0904b5c4278e4c2df3a9a334050ee61b87b0e
Svintsov Dmitry <root@uralbash.ru>
2016-10-06 23:10:09 +0500

    ++static

--- 4.static/1.paste_blog/default.nix
+++ 4.static/1.paste_blog/default.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  pythonPackages = pkgs.python35Packages;
+  php = pkgs.php;
   stdenv = pkgs.stdenv;
   python3 = pkgs.python35;
+  pythonPackages = pkgs.python35Packages;
 
   paste3 = pythonPackages.buildPythonPackage rec {
     name = "paste-2.0.3";
@@ -31,8 +32,6 @@ let
       sha256 = "1hn709w0bpfg77s7pqrvfd2wlqnjmmm5vn8gzqid40jkahn15lvh";
     };
 
-    buildInputs = with pythonPackages; [];
-
     doCheck = false; # some files required by the test seem to be missing
   };
 
@@ -54,6 +53,7 @@ let
       homepage = "http://github.com/lukearno/selector/";
     };
   };
+
   wsgi-basic-auth = pythonPackages.buildPythonPackage rec {
     name = "wsgi-basic-auth-1.0.4";
 
@@ -66,11 +66,48 @@ let
 
     doCheck = false; # some files required by the test seem to be missing
   };
+
+  markupSafe = pythonPackages.buildPythonPackage rec {
+    name = "MarkupSafe-0.9.3";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/MarkupSafe-0.9.3.tar.gz;
+      sha256 = "1grb90x0wn1r1wk5agqg631bb6ljs5c0q19i8dzvc0s4ca4ah93f";
+    };
+
+    doCheck = false; # some files required by the test seem to be missing
+  };
+
+  mako = pythonPackages.buildPythonPackage rec {
+    name = "Mako-1.0.4";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/Mako-1.0.4.tar.gz;
+      sha256 = "0nchpw6akfcsg8w6irjlx0gyzadc123hv4g47sijgnqd9nz9vngy";
+    };
+
+    buildInputs = with pythonPackages; [ markupSafe ];
+
+    doCheck = false; # some files required by the test seem to be missing
+  };
+
+  whitenoise = pythonPackages.buildPythonPackage rec {
+    name = "whitenoise-1.0.4";
+
+    src = pkgs.fetchurl {
+      url = mirror://pypi/P/Paste/whitenoise-3.2.2.tar.gz;
+      sha256 = "0h5lmsn3m4vif4g36bwcs7rzbby8glynlpcq3lncv8gpwgfpriz4";
+    };
+
+    doCheck = false; # some files required by the test seem to be missing
+  };
+
 in rec {
   pyEnv = stdenv.mkDerivation {
-    name = "py-paste";
-    buildInputs = [ stdenv python3 paste3 ]
-      ++ [ resolver selector wsgi-basic-auth ]
-      ++ [ pythonPackages.webob pythonPackages.ipdb ];
+    name = "py-jinja";
+    buildInputs = [ stdenv php python3 mako whitenoise ] ++
+                  [ pythonPackages.webob pythonPackages.ipdb ] ++
+                  [ pythonPackages.jinja2 pythonPackages.six ] ++
+                  [ paste3 selector resolver wsgi-basic-auth ];
   };
 }
