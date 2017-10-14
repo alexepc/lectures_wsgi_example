00a847b5ae98e13cdf6a3bef392fb8073ce55c41
Svintsov Dmitry <root@uralbash.ru>
2017-10-14 12:45:53 +0500

    fix up

--- 3.templates/jinja2/default.nix
+++ 3.templates/jinja2/default.nix
@@ -24,37 +24,37 @@ let
     };
   };
 
-  resolver = pythonPackages.buildPythonPackage rec {
-    name = "resolver-0.2.1";
-
-    src = pkgs.fetchurl {
-      url = mirror://pypi/P/Paste/resolver-0.2.1.tar.gz;
-      sha256 = "1hn709w0bpfg77s7pqrvfd2wlqnjmmm5vn8gzqid40jkahn15lvh";
-    };
-
-    buildInputs = with pythonPackages; [];
-
-    doCheck = false; # some files required by the test seem to be missing
-  };
-
-  selector = pythonPackages.buildPythonPackage rec {
-    name = "selector-0.10.1";
-
-    src = pkgs.fetchurl {
-      url = mirror://pypi/P/Paste/selector-0.10.1.tar.gz;
-      sha256 = "03sq1dhll6k84s6acb57wrbc0x959s83bp2r5fysja5x6dwsiqrc";
-    };
-
-    buildInputs = with pythonPackages; [ resolver ];
-
-    doCheck = false; # some files required by the test seem to be missing
-
-    meta = {
-      description = "This distribution provides WSGI middleware for “RESTful”
-      mapping of URL paths to WSGI applications";
-      homepage = "http://github.com/lukearno/selector/";
-    };
-  };
+  # resolver = pythonPackages.buildPythonPackage rec {
+  #   name = "resolver-0.2.1";
+  #
+  #   src = pkgs.fetchurl {
+  #     url = mirror://pypi/P/Paste/resolver-0.2.1.tar.gz;
+  #     sha256 = "1hn709w0bpfg77s7pqrvfd2wlqnjmmm5vn8gzqid40jkahn15lvh";
+  #   };
+  #
+  #   buildInputs = with pythonPackages; [];
+  #
+  #   doCheck = false; # some files required by the test seem to be missing
+  # };
+  #
+  # selector = pythonPackages.buildPythonPackage rec {
+  #   name = "selector-0.10.1";
+  #
+  #   src = pkgs.fetchurl {
+  #     url = mirror://pypi/P/Paste/selector-0.10.1.tar.gz;
+  #     sha256 = "03sq1dhll6k84s6acb57wrbc0x959s83bp2r5fysja5x6dwsiqrc";
+  #   };
+  #
+  #   buildInputs = with pythonPackages; [ resolver ];
+  #
+  #   doCheck = false; # some files required by the test seem to be missing
+  #
+  #   meta = {
+  #     description = "This distribution provides WSGI middleware for “RESTful”
+  #     mapping of URL paths to WSGI applications";
+  #     homepage = "http://github.com/lukearno/selector/";
+  #   };
+  # };
 
   wsgi-basic-auth = pythonPackages.buildPythonPackage rec {
     name = "wsgi-basic-auth-1.0.4";
@@ -73,7 +73,7 @@ let
     name = "MarkupSafe-0.9.3";
 
     src = pkgs.fetchurl {
-      url = mirror://pypi/P/Paste/MarkupSafe-0.9.3.tar.gz;
+      url = mirror://pypi/P/Paste/MarkupSafe-1.0.tar.gz;
       sha256 = "1grb90x0wn1r1wk5agqg631bb6ljs5c0q19i8dzvc0s4ca4ah93f";
     };
 
@@ -96,9 +96,18 @@ let
 in rec {
   pyEnv = stdenv.mkDerivation {
     name = "py-jinja";
-    buildInputs = [ stdenv php python3 mako ] ++
-                  [ pythonPackages.webob pythonPackages.ipdb ] ++
-                  [ pythonPackages.jinja2 pythonPackages.six ] ++
-                  [ paste3 selector resolver wsgi-basic-auth ];
+    buildInputs = [
+      stdenv
+      php
+      python3
+      # mako
+      pythonPackages.webob
+      pythonPackages.ipdb
+      pythonPackages.jinja2
+      pythonPackages.Mako
+      pythonPackages.six
+      paste3
+      # selector resolver wsgi-basic-auth
+    ];
   };
 }
